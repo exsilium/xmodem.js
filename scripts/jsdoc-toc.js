fa8c7d9696a5f6f8a60720dd46fc2d79673af1ce
Exile <exile@chamber.ee>
2017-06-26 19:26:19 +0000

    JSDoc generated content for gh-pages

---
+++ scripts/jsdoc-toc.js
@@ -0,0 +1,17 @@
+(function($) {
+    // TODO: make the node ID configurable
+    var treeNode = $('#jsdoc-toc-nav');
+
+    // initialize the tree
+    treeNode.tree({
+        autoEscape: false,
+        closedIcon: '&#x21e2;',
+        data: [{"label":"<a href=\"Xmodem.html\">Xmodem</a>","id":"Xmodem","children":[]}],
+        openedIcon: ' &#x21e3;',
+        saveState: true,
+        useContextMenu: false
+    });
+
+    // add event handlers
+    // TODO
+})(jQuery);
